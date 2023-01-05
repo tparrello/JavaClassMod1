e1fb7371b7e01c5036ee094ab9530b2f16ba7ace
tparrello <118761317+tparrello@users.noreply.github.com>
2023-01-05 15:01:27 -0600

    Finished

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -1,14 +1,26 @@
 public class Doubles {
     public static void main(String[] args) {
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+        
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2 \n);
+        
+        while (dice1 != dice2) {
+            dice1 = rollDice();
+            dice2 = rollDice();
 
-   //      See Learn the Part for instructions.
-       
-        int dice1;
-        int dice2;
-
-
+            System.out.println("Dice 1: " + dice1);
+            System.out.println("Dice 2: " + dice2 \n);
+        }
+        System.out.println("You rolled doubles!");
+    
     }
-
+    
+    public static int rollDice() {
+        double die = (Math.random() * 6) + 1;
+        return (int)die;
+    }
 }
 
 
