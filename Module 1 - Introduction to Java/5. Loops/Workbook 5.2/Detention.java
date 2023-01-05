772de328538e5c2204da79e8f8c93102aaa280b4
tparrello <118761317+tparrello@users.noreply.github.com>
2023-01-05 14:05:51 -0600

    Finished

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        String sentence = scan.nextLine();
+        for (i=0; i<99; i++) {
+            System.out.println(i+1 + ". " + sentence);
+        }
+            
         // See Learn the Part for the instructions.
-        
+        scan.close();
     }
 }
