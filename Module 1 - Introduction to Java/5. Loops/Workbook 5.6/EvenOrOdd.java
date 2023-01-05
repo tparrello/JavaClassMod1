78b5d3fc7071c9f56c51a198c6d13c46934f599f
tparrello <118761317+tparrello@users.noreply.github.com>
2023-01-05 14:25:15 -0600

    Finished

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
@@ -1,5 +1,13 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
+        for (int i = 0; i<=19; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " - even");   
+            } else {
+                System.out.print(i + " - odd");
+            }
+        }
+        
         // See Learn the Part for detailed instructions.
     }
 }
